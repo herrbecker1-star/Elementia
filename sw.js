82172281a3f6dc2a79cfd8064d04a7b4fddb7d07
herrbecker1-star <herrbecker1@gmail.com>
2026-08-23 18:06:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 //  Fassung holt. Wird sie vergessen, sitzt die halbe Klasse mit
 //  der alten App da und niemand versteht, warum.
 // ------------------------------------------------------------
-var FASSUNG = 14;
+var FASSUNG = 15;
 
 var GERUEST_SPEICHER = "elementia-geruest-" + FASSUNG;
 var BILD_SPEICHER    = "elementia-bilder-" + FASSUNG;
